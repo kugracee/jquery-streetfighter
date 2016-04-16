3638b4c9d861f297cd46b2ae1991a1f0a21481de
Grace Ku <grace.l.ku@gmail.com>
2016-04-16 12:08:34 -0400

    Fadein Fadeout

--- js/app.js
+++ js/app.js
@@ -23,7 +23,28 @@ $(document).ready(function() {
 	.mouseup(function() {
 		$('.ryu-throwing').hide();
 		$('ryu-ready').show();
-	});
+	// ryu goes back to his ready position
+	})
+
+ $(document).keydown(function(e) {
+    if (e.which == 88) {
+      $(".ryu-cool").show();
+      $(".ryu-still").hide();
+      $('.ryu-ready').hide();
+    };
+  }).keyup(function(e) {
+    if (e.which == 88) {
+      $(".ryu-cool").hide();
+      $(".ryu-still").show();
+    }
+  })
+
+$(document).click(function() {
+	$(".sf-logo").fadeIn(2000).fadeOut(2000);
+	$(".jquery-logo").delay(5000).fadeIn(2000).delay(900).fadeOut(2000);
+	$(".instructions, p").delay(5000).fadeIn(3000);
+});
+
 });
 
 function playHadouken() {
